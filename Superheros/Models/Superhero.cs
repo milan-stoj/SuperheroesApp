4e2e4316b329e91701f3b918df628aabc2430388
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-08-17 12:35:06 -0500

    init commit

---
+++ Superheros/Models/Superhero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superheros.Models
+{
+    public class Superhero
+    {
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+
+
+    }
+}
