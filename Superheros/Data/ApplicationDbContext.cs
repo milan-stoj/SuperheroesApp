0f51a62854e1299627830e3a45e87c65dd94f2a3
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-08-18 12:23:51 -0500

    finished migration

--- Superheros/Data/ApplicationDbContext.cs
+++ Superheros/Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Superheros.Models;
 
 namespace Superheros.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        DbSet<Superhero> Superhero { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
