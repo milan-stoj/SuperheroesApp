41bde2c030643bbd75a1659d0aa64833ce180bc8
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-08-18 22:27:00 -0500

    modified display names, and added image to main view

--- Superheroes/Models/Superhero.cs
+++ Superheroes/Models/Superhero.cs
@@ -10,10 +10,18 @@ namespace Superheroes.Models
     {
         [Key]
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        [Display(Name = "Alter Ego")]
         public string AlterEgo { get; set; }
+
+        [Display(Name = "Primary Ability")]
         public string PrimaryAbility { get; set; }
+
+        [Display(Name = "Secondary Ability")]
         public string SecondaryAbility { get; set; }
+
         public string Catchphrase { get; set; }
     }
 }
